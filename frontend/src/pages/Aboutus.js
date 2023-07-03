96f2457a21a14c5d10989194b1a7dcd647e71abe
Juho Lee <wngh417@gmail.com>
2023-07-03 14:47:52 +0100

    03.07-(1)

--- frontend/src/pages/Aboutus.js
+++ frontend/src/pages/Aboutus.js
@@ -16,138 +16,138 @@ export default function () {
         <div>
             <h1>About us</h1>
             <Box
-      sx={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        '& > :not(style)': {
-          m: 5,
-          width: 800,
-          height: 800,
-        },
-      }}
-    >
-      <Paper elevation={10}>
-        <h3>Contact Form</h3>
-        <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 2, width: '50' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <FormControl fullWidth sx={{ m: 1 }}>
-          <InputLabel htmlFor="outlined-adornment-amount">Your Name</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-amount"
-            label="Amount"
-          />
-        </FormControl>
-        <FormControl fullWidth sx={{ m: 1 }}>
-          <InputLabel htmlFor="outlined-adornment-amount">Email Address</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-amount"
-            label="Amount"
-          />
-        </FormControl>
-        <FormControl fullWidth sx={{ m: 1 }}>
-          <InputLabel htmlFor="outlined-adornment-amount">Your Name</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-amount"
-            label="Amount"
-          />
-        </FormControl>
-        
-       
+                sx={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    '& > :not(style)': {
+                        m: 5,
+                        width: 800,
+                        height: 800,
+                    },
+                }}
+            >
+                <Paper elevation={10}>
+                    <h3>Contact Form</h3>
+                    <Box
+                        component="form"
+                        sx={{
+                            '& > :not(style)': { m: 2, width: '50' },
+                        }}
+                        noValidate
+                        autoComplete="off"
+                    >
+                        <FormControl fullWidth sx={{ m: 1 }}>
+                            <InputLabel htmlFor="outlined-adornment-amount">Your Name</InputLabel>
+                            <OutlinedInput
+                                id="outlined-adornment-amount"
+                                label="Amount"
+                            />
+                        </FormControl>
+                        <FormControl fullWidth sx={{ m: 1 }}>
+                            <InputLabel htmlFor="outlined-adornment-amount">Email Address</InputLabel>
+                            <OutlinedInput
+                                id="outlined-adornment-amount"
+                                label="Amount"
+                            />
+                        </FormControl>
+                        <FormControl fullWidth sx={{ m: 1 }}>
+                            <InputLabel htmlFor="outlined-adornment-amount">Your Name</InputLabel>
+                            <OutlinedInput
+                                id="outlined-adornment-amount"
+                                label="Amount"
+                            />
+                        </FormControl>
 
 
-    </Box>
-      </Paper>
-      
-      <Paper elevation={10}> <Accordion>
-        <h3>FAQs</h3>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>People</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-          <ul>Alexander</ul>
-          <ul>Yui</ul>
-          <ul>Juho</ul>
-          <ul>Mark</ul>
-          <ul>Jeeshan</ul>
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-            <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>Why should we rate our modules?</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-          blalblalbllabllablalblablalblabl
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography>How do I add a modules that is not already in your database?</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            blalblalbllabllablalblablalblabl
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
 
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography>Will my ratings be anonymous?</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-          blalblalbllabllablalblablalblabl
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
 
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography>Contact us</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-          blalblalbllabllablalblablalblabl
-          </Typography>
-        </AccordionDetails>
-      </Accordion></Paper>
-    </Box>
-             
+                    </Box>
+                </Paper>
 
-      
+                <Paper elevation={10}> <Accordion>
+                    <h3>FAQs</h3>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                    >
+                        <Typography>People</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>
+                            <ul>Alexander</ul>
+                            <ul>Yui</ul>
+                            <ul>Juho</ul>
+                            <ul>Mark</ul>
+                            <ul>Jeeshan</ul>
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1a-content"
+                            id="panel1a-header"
+                        >
+                            <Typography>Why should we rate our modules?</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>
+                                blalblalbllabllablalblablalblabl
+                            </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel2a-content"
+                            id="panel2a-header"
+                        >
+                            <Typography>How do I add a modules that is not already in your database?</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>
+                                blalblalbllabllablalblablalblabl
+                            </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel2a-content"
+                            id="panel2a-header"
+                        >
+                            <Typography>Will my ratings be anonymous?</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>
+                                blalblalbllabllablalblablalblabl
+                            </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel2a-content"
+                            id="panel2a-header"
+                        >
+                            <Typography>Contact us</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>
+                                blalblalbllabllablalblablalblabl
+                            </Typography>
+                        </AccordionDetails>
+                    </Accordion></Paper>
+            </Box>
+
+
+
 
 
         </div>
-        
+
     )
 }
