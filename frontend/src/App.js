c6d89b5d79eb32f44a5c15b55b3bfab8aa479be4
yn27 <yn27@st-andrews.ac.uk>
2023-07-18 22:18:30 +0100

    Protect Survey Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 
-import React from "react";
-import { BrowserRouter, Route, Routes, Navigate }
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes, Navigate, useParams }
     from 'react-router-dom';
 
 import Home from "./pages/Home";
@@ -10,6 +10,39 @@ import Layout from "./compoments/Layout";
 import School from "./pages/School";
 import Module from "./pages/Module";
 import Survey from "./pages/Survey";
+import { getModuleFromCode } from "./api";
+
+
+const PrivateRoute = ({ children }) => {
+    const params = useParams()
+    const [module,setModule] = useState()
+    const [code,setCode] = useState()
+
+    useEffect(() => {
+        setModule(params.module)
+    },[params])
+
+    useEffect(() => {
+            getModuleFromCode(module)
+            .then(res => {
+                if (res.data !== undefined && res.data.length === 1) {
+                    console.log(res.data[0].secret_code)
+                    setCode(res.data[0].secret_code)
+                } else {
+                    console.log(res.data)
+                }
+            }).catch(err => {
+                console.log(`Module.js: ${err}`);
+            })
+        
+    },[module])
+
+    if (code == params.code) {
+        return children;
+    }else {
+      return  <Navigate to="/" replace={true} />
+    }
+}
 
 export default function App() {
 
@@ -23,7 +56,7 @@ export default function App() {
                         <Route path = "/leaderboard" element = {<LeaderBoard/>} />
                         <Route path = "/:school" element = {<School/>}/>
                         <Route path = "/:school/:module" element = {<Module/>}/>
-                        <Route path = "/:school/:module/survey/:code" element = {<Survey/>}/>
+                        <Route path = "/:school/:module/survey/:code" element = {<PrivateRoute><Survey/></PrivateRoute>}/>
                     </Route>
                 </Routes>
             </BrowserRouter>
